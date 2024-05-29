503082646ea099deea3091a74e588e4cfc1dc804
Matt Peterson <matt.peterson@swirldslabs.com>
2024-05-29 09:43:01 -0600

    Added a block-node module. confirmed build and clean work
    Signed-off-by: Matt Peterson <matt.peterson@swirldslabs.com>

--- block-node/src/main/java/com/hedera/hashgraph/Main.java
+++ block-node/src/main/java/com/hedera/hashgraph/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.hedera.hashgraph;
 
 public class Main {
     public static void main(String[] args) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "hedera-block-node"
 
+include("block-node")
