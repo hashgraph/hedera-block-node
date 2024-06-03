9e925a9e5dd40c325bf67db5bae6b10557de6753
Richard Bair <rbair23@users.noreply.github.com>
2024-06-03 15:14:04 -0700

    Initial gradle-based skeleton of a Helidon SE application with JPMS modules.

---
+++ settings.gradle.kts
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2022-2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("com.gradle.enterprise").version("3.15.1")
+}
+
+include(":server")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            // Compile time dependencies
+            version("io.helidon.webserver", "4.0.8")
+            version("io.helidon.webserver.http2", "4.0.8")
+            version("com.github.spotbugs.annotations", "4.7.3")
+
+            // Testing only versions
+            version("com.google.protobuf", "3.21.9")
+            version("org.assertj.core", "3.23.1")
+            version("org.junit.jupiter.api", "5.10.0")
+        }
+    }
+}
+
+// Build cache configuration
+val isCiServer = System.getenv().containsKey("CI")
+val gradleCacheUsername: String? = System.getenv("GRADLE_CACHE_USERNAME")
+val gradleCachePassword: String? = System.getenv("GRADLE_CACHE_PASSWORD")
+val gradleCacheAuthorized =
+    (gradleCacheUsername?.isNotEmpty() ?: false) && (gradleCachePassword?.isNotEmpty() ?: false)
+
+buildCache {
+    remote<HttpBuildCache> {
+        url = uri("https://cache.gradle.hedera.svcs.eng.swirldslabs.io/cache/")
+        isPush = isCiServer && gradleCacheAuthorized
+
+        isUseExpectContinue = true
+        isEnabled = !gradle.startParameter.isOffline
+
+        if (isCiServer && gradleCacheAuthorized) {
+            credentials {
+                username = gradleCacheUsername
+                password = gradleCachePassword
+            }
+        }
+    }
+}
