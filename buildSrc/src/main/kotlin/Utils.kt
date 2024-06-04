76544e224abad678785428d34bcbb1c74d176f51
Richard Bair <rbair23@users.noreply.github.com>
2024-06-04 13:17:22 -0700

    Fix copyright and .gitignore (#9)

--- buildSrc/src/main/kotlin/Utils.kt
+++ buildSrc/src/main/kotlin/Utils.kt
@@ -5,7 +5,7 @@ import java.io.OutputStream
 import java.io.PrintStream
 
 /*
- * Copyright (C) 2022 Hedera Hashgraph, LLC
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- buildSrc/src/main/kotlin/com.hedera.block.spotless-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.hedera.block.spotless-conventions.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 Hedera Hashgraph, LLC
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,15 +35,6 @@ spotless {
 
     format("actionYaml") {
         target(".github/workflows/*.yaml")
-        /*
-         * Prettier requires NodeJS and NPM installed; however, the NodeJS Gradle plugin and Spotless do not yet
-         * integrate with each other. Currently there is an open issue report against spotless.
-         *
-         *   *** Please see for more information: https://github.com/diffplug/spotless/issues/728 ***
-         *
-         * The workaround provided in the above issue does not work in Gradle 7.5+ and therefore is not a viable solution.
-         */
-        // prettier()
 
         trimTrailingWhitespace()
         indentWithSpaces()
