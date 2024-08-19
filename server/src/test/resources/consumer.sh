a255990101fa5908f387a0d4f87298a2d1ca3493
Matt Peterson <matt.peterson@swirldslabs.com>
2024-08-19 16:45:37 -0600

    fix: able to subscribe to the stream
    Signed-off-by: Matt Peterson <matt.peterson@swirldslabs.com>

--- server/src/test/resources/consumer.sh
+++ server/src/test/resources/consumer.sh
@@ -15,11 +15,12 @@ echo "Param is: $1"
 
 # Use environment variables or default values
 GRPC_SERVER=${GRPC_SERVER:-"localhost:8080"}
-GRPC_METHOD=${GRPC_METHOD:-"BlockStreamGrpcService/subscribeBlockStream"}
-PATH_TO_PROTO=${PATH_TO_PROTO:-"../../../../protos/src/main/protobuf/blockstream.proto"}
-PROTO_IMPORT_PATH=${PROTO_IMPORT_PATH:-"../../../../protos/src/main/protobuf"}
+GRPC_METHOD=${GRPC_METHOD:-"com.hedera.hapi.block.BlockStreamService/subscribeBlockStream"}
+#PATH_TO_PROTO=${PATH_TO_PROTO:-"../../../../protos/src/main/protobuf/blockstream.proto"}
+PATH_TO_PROTO="./block_service.proto"
+#PROTO_IMPORT_PATH=${PROTO_IMPORT_PATH:-"../../../../protos/src/main/protobuf"}
 
 echo "Starting consumer..."
 
 # Response block messages from the gRPC server are printed to stdout.
-echo "{\"start_block_number\": $1}" | grpcurl -plaintext -import-path $PROTO_IMPORT_PATH -proto $PATH_TO_PROTO -d @ $GRPC_SERVER $GRPC_METHOD
+echo "{\"start_block_number\": $1}" | grpcurl -plaintext -proto $PATH_TO_PROTO -d @ $GRPC_SERVER $GRPC_METHOD
