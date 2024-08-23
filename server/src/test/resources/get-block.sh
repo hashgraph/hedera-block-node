66243f2fa2c2e7c935972767d4bf822ff6c71484
Alfredo Gutierrez <alfredo@swirldslabs.com>
2024-08-23 15:37:54 -0600

    Fix smoke test after PBJ Changes were merged and rebased
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- server/src/test/resources/get-block.sh
+++ server/src/test/resources/get-block.sh
@@ -15,11 +15,10 @@ echo "Param is: $1"
 
 # Use environment variables or default values
 GRPC_SERVER=${GRPC_SERVER:-"localhost:8080"}
-GRPC_METHOD=${GRPC_METHOD:-"BlockStreamGrpcService/singleBlock"}
-PATH_TO_PROTO=${PATH_TO_PROTO:-"../../../../protos/src/main/protobuf/blockstream.proto"}
-PROTO_IMPORT_PATH=${PROTO_IMPORT_PATH:-"../../../../protos/src/main/protobuf"}
+GRPC_METHOD=${GRPC_METHOD:-"com.hedera.hapi.block.BlockStreamService/singleBlock"}
+PATH_TO_PROTO="./block_service.proto"
 
 echo "Requesting block $1..."
 
 # Response block messages from the gRPC server are printed to stdout.
-echo "{\"block_number\": $1}" | grpcurl -plaintext -import-path $PROTO_IMPORT_PATH -proto $PATH_TO_PROTO -d @ $GRPC_SERVER $GRPC_METHOD
+echo "{\"block_number\": $1}" | grpcurl -plaintext -proto $PATH_TO_PROTO -d @ $GRPC_SERVER $GRPC_METHOD
