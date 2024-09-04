0d9f422eed617622e7691172f9facaa661e4a683
Alfredo Gutierrez <alfredo@swirldslabs.com>
2024-09-04 10:25:57 -0600

    changing default to DIR
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- simulator/src/test/java/com/hedera/block/simulator/config/ConfigInjectionModuleTest.java
+++ simulator/src/test/java/com/hedera/block/simulator/config/ConfigInjectionModuleTest.java
@@ -52,7 +52,7 @@ class ConfigInjectionModuleTest {
                 ConfigInjectionModule.provideBlockStreamConfig(configuration);
 
         Assertions.assertNotNull(blockStreamConfig);
-        Assertions.assertEquals(GenerationMode.ADHOC, blockStreamConfig.generationMode());
+        Assertions.assertEquals(GenerationMode.DIR, blockStreamConfig.generationMode());
     }
 
     @Test
