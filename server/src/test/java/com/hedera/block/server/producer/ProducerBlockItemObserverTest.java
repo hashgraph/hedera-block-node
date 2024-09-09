9af0e7fc566542dd8b774df572fb05cddfdc42e0
Matt Peterson <matt.peterson@swirldslabs.com>
2024-09-09 14:05:24 -0600

    fix: removed tests that no longer apply. will move that coverage to IT tests
    Signed-off-by: Matt Peterson <matt.peterson@swirldslabs.com>

--- server/src/test/java/com/hedera/block/server/producer/ProducerBlockItemObserverTest.java
+++ server/src/test/java/com/hedera/block/server/producer/ProducerBlockItemObserverTest.java
@@ -17,13 +17,8 @@
 package com.hedera.block.server.producer;
 
 import static com.hedera.block.server.Translator.fromPbj;
-import static com.hedera.block.server.metrics.BlockNodeMetricTypes.Counter.LiveBlockItems;
-import static com.hedera.block.server.producer.Util.getFakeHash;
 import static com.hedera.block.server.util.PersistTestUtils.generateBlockItems;
 import static com.hedera.block.server.util.PersistTestUtils.reverseByteArray;
-import static com.hedera.block.server.util.TestConfigUtil.CONSUMER_TIMEOUT_THRESHOLD_KEY;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
@@ -31,34 +26,21 @@ import static org.mockito.Mockito.when;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.hedera.block.server.ServiceStatus;
-import com.hedera.block.server.ServiceStatusImpl;
 import com.hedera.block.server.config.BlockNodeContext;
-import com.hedera.block.server.consumer.ConsumerConfig;
-import com.hedera.block.server.consumer.ConsumerStreamResponseObserver;
 import com.hedera.block.server.data.ObjectEvent;
-import com.hedera.block.server.mediator.LiveStreamMediatorBuilder;
 import com.hedera.block.server.mediator.StreamMediator;
 import com.hedera.block.server.mediator.SubscriptionHandler;
 import com.hedera.block.server.persistence.storage.write.BlockWriter;
 import com.hedera.block.server.util.TestConfigUtil;
-import com.hedera.hapi.block.Acknowledgement;
 import com.hedera.hapi.block.EndOfStream;
-import com.hedera.hapi.block.ItemAcknowledgement;
-import com.hedera.hapi.block.PublishStreamRequest;
 import com.hedera.hapi.block.PublishStreamResponse;
 import com.hedera.hapi.block.PublishStreamResponseCode;
 import com.hedera.hapi.block.SubscribeStreamResponse;
 import com.hedera.hapi.block.stream.BlockItem;
-import com.hedera.hapi.block.stream.output.BlockHeader;
-import com.hedera.pbj.runtime.io.buffer.Bytes;
-import edu.umd.cs.findbugs.annotations.NonNull;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.time.InstantSource;
 import java.util.List;
-import java.util.Map;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -91,127 +73,6 @@ public class ProducerBlockItemObserverTest {
     private static final int testTimeout = 1000;
 
     @Test
-    @Disabled
-    public void testProducerOnNext() throws IOException, NoSuchAlgorithmException {
-
-        final BlockNodeContext blockNodeContext = TestConfigUtil.getTestBlockNodeContext();
-        final List<BlockItem> blockItems = generateBlockItems(1);
-        final ProducerBlockItemObserver producerBlockItemObserver =
-                new ProducerBlockItemObserver(
-                        testClock,
-                        streamMediator,
-                        subscriptionHandler,
-                        publishStreamResponseObserver,
-                        blockNodeContext,
-                        serviceStatus);
-
-        when(serviceStatus.isRunning()).thenReturn(true);
-
-        final BlockItem blockHeader = blockItems.getFirst();
-        final PublishStreamRequest publishStreamRequest =
-                PublishStreamRequest.newBuilder().blockItem(blockHeader).build();
-        producerBlockItemObserver.onNext(fromPbj(publishStreamRequest));
-
-        verify(streamMediator, timeout(testTimeout).times(1)).publish(blockHeader);
-
-        final Acknowledgement ack = buildAck(blockHeader);
-        final PublishStreamResponse publishStreamResponse =
-                PublishStreamResponse.newBuilder().acknowledgement(ack).build();
-
-        verify(publishStreamResponseObserver, timeout(testTimeout).times(1))
-                .onNext(fromPbj(publishStreamResponse));
-
-        // Helidon will call onCompleted after onNext
-        producerBlockItemObserver.onCompleted();
-
-        verify(publishStreamResponseObserver, timeout(testTimeout).times(1)).onCompleted();
-    }
-
-    @Test
-    @Disabled
-    public void testProducerWithManyConsumers() throws IOException {
-
-        final BlockNodeContext blockNodeContext = TestConfigUtil.getTestBlockNodeContext();
-        final var streamMediator =
-                LiveStreamMediatorBuilder.newBuilder(blockNodeContext, new ServiceStatusImpl())
-                        .build();
-
-        // Mock a clock with 2 different return values in response to anticipated
-        // millis() calls. Here the second call will always be inside the timeout window.
-        final BlockNodeContext testContext =
-                TestConfigUtil.getTestBlockNodeContext(
-                        Map.of(CONSUMER_TIMEOUT_THRESHOLD_KEY, "100"));
-        final ConsumerConfig consumerConfig =
-                testContext.configuration().getConfigData(ConsumerConfig.class);
-
-        long TEST_TIME = 1_719_427_664_950L;
-        when(testClock.millis())
-                .thenReturn(TEST_TIME, TEST_TIME + consumerConfig.timeoutThresholdMillis());
-
-        final var concreteObserver1 =
-                new ConsumerStreamResponseObserver(
-                        testClock, streamMediator, streamObserver1, testContext);
-
-        final var concreteObserver2 =
-                new ConsumerStreamResponseObserver(
-                        testClock, streamMediator, streamObserver2, testContext);
-
-        final var concreteObserver3 =
-                new ConsumerStreamResponseObserver(
-                        testClock, streamMediator, streamObserver3, testContext);
-
-        // Set up the subscribers
-        streamMediator.subscribe(concreteObserver1);
-        streamMediator.subscribe(concreteObserver2);
-        streamMediator.subscribe(concreteObserver3);
-
-        assertTrue(
-                streamMediator.isSubscribed(concreteObserver1),
-                "Expected the mediator to have observer1 subscribed");
-        assertTrue(
-                streamMediator.isSubscribed(concreteObserver2),
-                "Expected the mediator to have observer2 subscribed");
-        assertTrue(
-                streamMediator.isSubscribed(concreteObserver3),
-                "Expected the mediator to have observer3 subscribed");
-
-        final BlockHeader blockHeader = BlockHeader.newBuilder().number(1).build();
-        final BlockItem blockItem = BlockItem.newBuilder().blockHeader(blockHeader).build();
-        final SubscribeStreamResponse subscribeStreamResponse =
-                SubscribeStreamResponse.newBuilder().blockItem(blockItem).build();
-
-        when(serviceStatus.isRunning()).thenReturn(true);
-
-        final ProducerBlockItemObserver producerBlockItemObserver =
-                new ProducerBlockItemObserver(
-                        testClock,
-                        streamMediator,
-                        subscriptionHandler,
-                        publishStreamResponseObserver,
-                        blockNodeContext,
-                        serviceStatus);
-
-        final PublishStreamRequest publishStreamRequest =
-                PublishStreamRequest.newBuilder().blockItem(blockItem).build();
-        producerBlockItemObserver.onNext(fromPbj(publishStreamRequest));
-
-        // Confirm the block item counter was incremented
-        assertEquals(1, blockNodeContext.metricsService().get(LiveBlockItems).get());
-
-        // Confirm each subscriber was notified of the new block
-        verify(streamObserver1, timeout(testTimeout).times(1))
-                .onNext(fromPbj(subscribeStreamResponse));
-        verify(streamObserver2, timeout(testTimeout).times(1))
-                .onNext(fromPbj(subscribeStreamResponse));
-        verify(streamObserver3, timeout(testTimeout).times(1))
-                .onNext(fromPbj(subscribeStreamResponse));
-
-        // Confirm the BlockStorage write method was
-        // called despite the absence of subscribers
-        verify(blockWriter).write(blockItem);
-    }
-
-    @Test
     public void testOnError() throws IOException {
 
         final BlockNodeContext blockNodeContext = TestConfigUtil.getTestBlockNodeContext();
@@ -228,35 +89,6 @@ public class ProducerBlockItemObserverTest {
         producerBlockItemObserver.onError(t);
         verify(publishStreamResponseObserver).onError(t);
     }
-
-    //    @Test
-    //    public void testItemAckBuilderExceptionTest() throws IOException {
-    //
-    //        when(serviceStatus.isRunning()).thenReturn(true);
-    //
-    //        final BlockNodeContext blockNodeContext = TestConfigUtil.getTestBlockNodeContext();
-    //        final ProducerBlockItemObserver testProducerBlockItemObserver =
-    //                new TestProducerBlockItemObserver(
-    //                        streamMediator,
-    //                        publishStreamResponseObserver,
-    //                        blockNodeContext,
-    //                        serviceStatus);
-    //
-    //        final List<BlockItem> blockItems = generateBlockItems(1);
-    //        final BlockItem blockHeader = blockItems.getFirst();
-    //        final PublishStreamRequest publishStreamRequest =
-    //                PublishStreamRequest.newBuilder().blockItem(blockHeader).build();
-    //        testProducerBlockItemObserver.onNext(fromPbj(publishStreamRequest));
-    //
-    //        final EndOfStream endOfStream =
-    //                EndOfStream.newBuilder()
-    //                        .status(PublishStreamResponseCode.STREAM_ITEMS_UNKNOWN)
-    //                        .build();
-    //        final PublishStreamResponse errorResponse =
-    //                PublishStreamResponse.newBuilder().status(endOfStream).build();
-    //        verify(publishStreamResponseObserver, timeout(testTimeout).times(1))
-    //                .onNext(fromPbj(errorResponse));
-    //    }
 
     @Test
     public void testBlockItemThrowsParseException()
@@ -307,29 +139,5 @@ public class ProducerBlockItemObserverTest {
                 .onNext(fromPbj(PublishStreamResponse.newBuilder().status(endOfStream).build()));
 
         verify(serviceStatus, timeout(testTimeout).times(1)).stopWebServer();
-    }
-
-    //    private static class TestProducerBlockItemObserver extends ProducerBlockItemObserver {
-    //        public TestProducerBlockItemObserver(
-    //                final StreamMediator<BlockItem, ObjectEvent<SubscribeStreamResponse>>
-    //                        streamMediator,
-    //                final StreamObserver<com.hedera.hapi.block.protoc.PublishStreamResponse>
-    //                        publishStreamResponseObserver,
-    //                final BlockNodeContext blockNodeContext,
-    //                final ServiceStatus serviceStatus) {
-    //            super(streamMediator, publishStreamResponseObserver, blockNodeContext,
-    // serviceStatus);
-    //        }
-    //    }
-
-    @NonNull
-    private static Acknowledgement buildAck(@NonNull final BlockItem blockItem)
-            throws NoSuchAlgorithmException {
-        ItemAcknowledgement itemAck =
-                ItemAcknowledgement.newBuilder()
-                        .itemHash(Bytes.wrap(getFakeHash(blockItem)))
-                        .build();
-
-        return Acknowledgement.newBuilder().itemAck(itemAck).build();
     }
 }
