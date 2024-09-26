f574b1ac2ca5be536b96b4efb40133a20d3fb92b
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-09-27 00:54:25 +0200

    build: use common Gradle config
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -15,7 +15,13 @@
  */
 
 plugins {
-    id("application")
-    id("com.hedera.block.conventions")
-    id("me.champeau.jmh")
+    id("org.hiero.gradle.report.code-coverage")
+    id("org.hiero.gradle.check.spotless")
+    id("org.hiero.gradle.check.spotless-kotlin")
+}
+
+dependencies {
+    implementation(project(":server"))
+    implementation(project(":simulator"))
+    implementation(project(":suites"))
 }
