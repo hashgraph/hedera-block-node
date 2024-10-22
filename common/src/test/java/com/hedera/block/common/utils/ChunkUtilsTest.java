925b9f7d77af52ccd62b2a5c166a5beb65010220
Alfredo Gutierrez <alfredo@swirldslabs.com>
2024-10-22 11:14:26 -0600

    replaced with single import
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- common/src/test/java/com/hedera/block/common/utils/ChunkUtilsTest.java
+++ common/src/test/java/com/hedera/block/common/utils/ChunkUtilsTest.java
@@ -16,7 +16,9 @@
 
 package com.hedera.block.common.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
