f0b0bee42739afebfb28161b41bd9e417e1474b7
Matt Peterson <matt.peterson@swirldslabs.com>
2024-10-28 14:49:54 -0600

    fix: changed the smoke tests to try the health and readiness endpoints first
    Signed-off-by: Matt Peterson <matt.peterson@swirldslabs.com>

--- server/src/test/resources/smoke-test.sh
+++ server/src/test/resources/smoke-test.sh
@@ -58,12 +58,31 @@ else
     echo "No log file provided, skipping startup pattern check. LOG_FILE is: $LOG_FILE"
 fi
 
-# 2. Start the consumer script with parameters 1 1000 and save logs to consumer.log
+# 2. Call the endpoints /health/livez and /health/readyz
+SERVER_URL="http://localhost:8080"
+LIVENESS_ENDPOINT="/healthz/livez"
+READINESS_ENDPOINT="/healthz/readyz"
+
+if ! curl --retry 5 --retry-delay 10 -f $SERVER_URL$LIVENESS_ENDPOINT; then
+    echo "$LIVENESS_ENDPOINT failed."
+    shutdown
+    exit 1
+fi
+echo "$LIVENESS_ENDPOINT endpoint is healthy."
+
+if ! curl --retry 5 --retry-delay 10 -f $SERVER_URL$READINESS_ENDPOINT; then
+    echo "$READINESS_ENDPOINT endpoint failed."
+    shutdown
+    exit 1
+fi
+echo "$READINESS_ENDPOINT endpoint is ready."
+
+# 3. Start the consumer script with parameters 1 1000 and save logs to consumer.log
 ./consumer.sh 1 1000 > consumer.log 2>&1 &
 CONSUMER_PID=$!
 echo "Started consumer with PID $CONSUMER_PID, logging to consumer.log"
 
-# 3. Start the producer script with parameter 1 and save logs to producer.log
+# 4. Start the producer script with parameter 1 and save logs to producer.log
 ./producer.sh 1 > producer.log 2>&1 &
 PRODUCER_PID=$!
 echo "Started producer with PID $PRODUCER_PID, logging to producer.log"
@@ -71,7 +90,7 @@ echo "Started producer with PID $PRODUCER_PID, logging to producer.log"
 # Sleep time after starting the consumer
 sleep 5  # Adjust sleep time as needed
 
-# 4. Run the get-block script with parameter 1 and save logs to get-block.log
+# 5. Run the get-block script with parameter 1 and save logs to get-block.log
 if ! ./get-block.sh 1 > get-block.log 2>&1; then
     echo "get-block.sh failed."
     echo "get-block logs:"
@@ -80,25 +99,6 @@ if ! ./get-block.sh 1 > get-block.log 2>&1; then
     exit 1
 fi
 echo "get-block.sh executed successfully."
-
-# 5. Call the endpoints /health/livez and /health/readyz
-SERVER_URL="http://localhost:8080"
-LIVENESS_ENDPOINT="/healthz/livez"
-READINESS_ENDPOINT="/healthz/readyz"
-
-if ! curl -f $SERVER_URL$LIVENESS_ENDPOINT; then
-    echo "$LIVENESS_ENDPOINT failed."
-    shutdown
-    exit 1
-fi
-echo "$LIVENESS_ENDPOINT endpoint is healthy."
-
-if ! curl -f $SERVER_URL$READINESS_ENDPOINT; then
-    echo "$READINESS_ENDPOINT endpoint failed."
-    shutdown
-    exit 1
-fi
-echo "$READINESS_ENDPOINT endpoint is ready."
 
 # 6. Shut everything down
 shutdown
