bb3c08be0a5f6b96fc32d9e40864403d7cdde735
georgi-l95 <glazarov95@gmail.com>
2024-10-29 11:06:42 +0200

    spotless apply
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- simulator/src/main/java/com/hedera/block/simulator/Constants.java
+++ simulator/src/main/java/com/hedera/block/simulator/Constants.java
@@ -24,10 +24,11 @@ public final class Constants {
     /** postfix for gzip files */
     public static final String GZ_EXTENSION = ".gz";
 
-    /** Constructor to prevent instantiation. this is only a utility class */
-    private Constants() {}
     /**
      * Used for converting nanoseconds to milliseconds and vice versa
      */
     public static final int NANOS_PER_MILLI = 1_000_000;
+
+    /** Constructor to prevent instantiation. this is only a utility class */
+    private Constants() {}
 }
