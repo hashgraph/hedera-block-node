b97a55a31c1f28165dd65ffa8e364b2610846801
jasperpotts <jasperpotts@users.noreply.github.com>
2024-10-30 15:18:43 -0700

    Removed Comment
    Signed-off-by: jasperpotts <jasperpotts@users.noreply.github.com>

--- buildSrc/src/main/kotlin/com.hedera.block.tools.gradle.kts
+++ buildSrc/src/main/kotlin/com.hedera.block.tools.gradle.kts
@@ -18,8 +18,4 @@ plugins {
     id("application")
     id("com.hedera.block.conventions")
     id("me.champeau.jmh")
-} //
-// val maven = publishing.publications.create<MavenPublication>("maven") { from(components["java"])
-// }
-//
-// signing.sign(maven)
+}
