66eda87e32fe543dbfd15f8ccdd331304beda9d9
Atanas Atanasov <a.v.atanasov98@gmail.com>
2024-11-05 12:51:27 +0200

    attempt to resolve smoke tests with retry in curl
    Signed-off-by: Atanas Atanasov <a.v.atanasov98@gmail.com>

--- server/src/test/resources/smoke-test.sh
+++ server/src/test/resources/smoke-test.sh
@@ -2,7 +2,6 @@
 
 # Variable to track whether shutdown has been called
 SHUTDOWN_CALLED=0
-
 # Function to shut down the background processes
 shutdown() {
     if [[ $SHUTDOWN_CALLED -eq 1 ]]; then
@@ -42,52 +41,36 @@ shutdown() {
 # Trap any exit signal to ensure everything is cleaned up
 trap "shutdown; exit 1" ERR SIGINT SIGTERM
 
-
-# 1. Verify that the logs have the startup pattern (only if log file is provided)
-LOG_FILE=$1
-STARTUP_PATTERN="Block Node Server started at port"
-
-if [[ -n "$LOG_FILE" ]]; then
-    if grep -q "$STARTUP_PATTERN" "$LOG_FILE"; then
-        echo "Startup pattern found in logs."
-    else
-        echo "Startup pattern not found in logs."
-        exit 1
-    fi
-else
-    echo "No log file provided, skipping startup pattern check. LOG_FILE is: $LOG_FILE"
-fi
-
-# 2. Call the endpoints /health/livez and /health/readyz
+# 1. Call the endpoints /health/livez and /health/readyz
 SERVER_URL="http://localhost:8080"
 LIVENESS_ENDPOINT="/healthz/livez"
 READINESS_ENDPOINT="/healthz/readyz"
 
-if ! curl -v --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 $SERVER_URL$LIVENESS_ENDPOINT; then
+if ! curl -v --connect-timeout 5 --max-time 10 --retry 12 --retry-connrefused --retry-delay 5 $SERVER_URL$LIVENESS_ENDPOINT; then
     echo "$LIVENESS_ENDPOINT failed."
     shutdown
     exit 1
 fi
 echo "$LIVENESS_ENDPOINT endpoint is healthy."
 
-if ! curl -v --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 $SERVER_URL$READINESS_ENDPOINT; then
+if ! curl -v --connect-timeout 5 --max-time 10 --retry 12 --retry-connrefused --retry-delay 5 $SERVER_URL$READINESS_ENDPOINT; then
     echo "$READINESS_ENDPOINT endpoint failed."
     shutdown
     exit 1
 fi
 echo "$READINESS_ENDPOINT endpoint is ready."
 
-# 3. Start the consumer script with parameters 1 1000 and save logs to consumer.log
+# 2. Start the consumer script with parameters 1 1000 and save logs to consumer.log
 ./consumer.sh 1 1000 > consumer.log 2>&1 &
 CONSUMER_PID=$!
 echo "Started consumer with PID $CONSUMER_PID, logging to consumer.log"
 
-# 4. Start the producer script with parameter 1 and save logs to producer.log
+# 3. Start the producer script with parameter 1 and save logs to producer.log
 ./producer.sh 1 > producer.log 2>&1 &
 PRODUCER_PID=$!
 echo "Started producer with PID $PRODUCER_PID, logging to producer.log"
 
-# Sleep time after starting the consumer
+# 4. Sleep time after starting the consumer and producer scripts
 sleep 5  # Adjust sleep time as needed
 
 # 5. Run the get-block script with parameter 1 and save logs to get-block.log
