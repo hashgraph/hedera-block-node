76ffd945968d1ecf3926150502a6c5d61cee714c
Alfredo Gutierrez <alfredo@swirldslabs.com>
2024-12-17 12:46:00 -0600

    adding some extra exception handling.
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- server/src/main/java/com/hedera/block/server/verification/StreamVerificationHandlerImpl.java
+++ server/src/main/java/com/hedera/block/server/verification/StreamVerificationHandlerImpl.java
@@ -17,6 +17,7 @@
 package com.hedera.block.server.verification;
 
 import static com.hedera.block.server.metrics.BlockNodeMetricTypes.Counter.StreamPersistenceHandlerError;
+import static com.hedera.block.server.metrics.BlockNodeMetricTypes.Counter.VerificationBlocksError;
 import static java.lang.System.Logger.Level.DEBUG;
 import static java.lang.System.Logger.Level.ERROR;
 
@@ -24,7 +25,9 @@ import com.hedera.block.server.events.BlockNodeEventHandler;
 import com.hedera.block.server.events.ObjectEvent;
 import com.hedera.block.server.exception.BlockStreamProtocolException;
 import com.hedera.block.server.mediator.SubscriptionHandler;
+import com.hedera.block.server.metrics.BlockNodeMetricTypes;
 import com.hedera.block.server.metrics.MetricsService;
+import com.hedera.block.server.notifier.Notifier;
 import com.hedera.block.server.service.ServiceStatus;
 import com.hedera.hapi.block.BlockItemUnparsed;
 import com.hedera.hapi.block.SubscribeStreamResponseUnparsed;
@@ -40,6 +43,7 @@ public class StreamVerificationHandlerImpl
 
     private final System.Logger LOGGER = System.getLogger(getClass().getName());
     private final SubscriptionHandler<SubscribeStreamResponseUnparsed> subscriptionHandler;
+    private final Notifier notifier;
     private final MetricsService metricsService;
     private final ServiceStatus serviceStatus;
     private final BlockVerificationService blockVerificationService;
@@ -50,11 +54,13 @@ public class StreamVerificationHandlerImpl
     @Inject
     public StreamVerificationHandlerImpl(
             @NonNull final SubscriptionHandler<SubscribeStreamResponseUnparsed> subscriptionHandler,
+            @NonNull final Notifier notifier,
             @NonNull final MetricsService metricsService,
             @NonNull final ServiceStatus serviceStatus,
             @NonNull final BlockVerificationService blockVerificationService) {
 
         this.subscriptionHandler = subscriptionHandler;
+        this.notifier = notifier;
         this.metricsService = metricsService;
         this.serviceStatus = serviceStatus;
         this.blockVerificationService = blockVerificationService;
@@ -62,34 +68,50 @@ public class StreamVerificationHandlerImpl
 
     @Override
     public void onEvent(ObjectEvent<SubscribeStreamResponseUnparsed> event, long l, boolean b) throws Exception {
-        if (!serviceStatus.isRunning()) {
-            LOGGER.log(ERROR, "Service is not running. Block item will not be processed further.");
-        }
 
-        final SubscribeStreamResponseUnparsed subscribeStreamResponse = event.get();
-        final OneOf<SubscribeStreamResponseUnparsed.ResponseOneOfType> oneOfTypeOneOf =
-                subscribeStreamResponse.response();
-        switch (oneOfTypeOneOf.kind()) {
-            case BLOCK_ITEMS -> {
-                if (subscribeStreamResponse.blockItems() == null) {
-                    final String message = PROTOCOL_VIOLATION_MESSAGE.formatted(
-                            "SubscribeStreamResponse", "BLOCK_ITEM", "block_item", subscribeStreamResponse);
+        try {
+
+            if (!serviceStatus.isRunning()) {
+                LOGGER.log(ERROR, "Service is not running. Block item will not be processed further.");
+            }
+
+            final SubscribeStreamResponseUnparsed subscribeStreamResponse = event.get();
+            final OneOf<SubscribeStreamResponseUnparsed.ResponseOneOfType> oneOfTypeOneOf =
+                    subscribeStreamResponse.response();
+            switch (oneOfTypeOneOf.kind()) {
+                case BLOCK_ITEMS -> {
+                    if (subscribeStreamResponse.blockItems() == null) {
+                        final String message = PROTOCOL_VIOLATION_MESSAGE.formatted(
+                                "SubscribeStreamResponse", "BLOCK_ITEM", "block_item", subscribeStreamResponse);
+                        LOGGER.log(ERROR, message);
+                        metricsService.get(VerificationBlocksError).increment();
+                        throw new BlockStreamProtocolException(message);
+                    } else {
+                        List<BlockItemUnparsed> blockItems =
+                                subscribeStreamResponse.blockItems().blockItems();
+                        blockVerificationService.onBlockItemsReceived(blockItems);
+                    }
+                }
+                case STATUS -> LOGGER.log(DEBUG, "Unexpected received a status message rather than a block item");
+                default -> {
+                    final String message = "Unknown response type: " + oneOfTypeOneOf.kind();
                     LOGGER.log(ERROR, message);
-                    metricsService.get(StreamPersistenceHandlerError).increment();
+                    metricsService.get(VerificationBlocksError).increment();
                     throw new BlockStreamProtocolException(message);
-                } else {
-                    List<BlockItemUnparsed> blockItems =
-                            subscribeStreamResponse.blockItems().blockItems();
-                    blockVerificationService.onBlockItemsReceived(blockItems);
                 }
             }
-            case STATUS -> LOGGER.log(DEBUG, "Unexpected received a status message rather than a block item");
-            default -> {
-                final String message = "Unknown response type: " + oneOfTypeOneOf.kind();
-                LOGGER.log(ERROR, message);
-                metricsService.get(StreamPersistenceHandlerError).increment();
-                throw new BlockStreamProtocolException(message);
-            }
+        } catch(final Exception e) {
+
+            LOGGER.log(ERROR, "Failed to verify BlockItems: ", e);
+            // Trigger the server to stop accepting new requests
+            serviceStatus.stopRunning(getClass().getName());
+
+            // Unsubscribe from the mediator to avoid additional onEvent calls.
+            subscriptionHandler.unsubscribe(this);
+
+            // Broadcast the problem to the notifier
+            notifier.notifyUnrecoverableError();
+
         }
     }
 }
