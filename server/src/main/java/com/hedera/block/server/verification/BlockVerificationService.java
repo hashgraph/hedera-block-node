0244bc044458374efdda4e2e8a61ca6377ac7869
Alfredo Gutierrez <alfredo@swirldslabs.com>
2024-12-16 23:01:36 -0600

    added javadoc to BlockVerificationService interface
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- server/src/main/java/com/hedera/block/server/verification/BlockVerificationService.java
+++ server/src/main/java/com/hedera/block/server/verification/BlockVerificationService.java
@@ -20,6 +20,15 @@ import com.hedera.hapi.block.BlockItemUnparsed;
 import com.hedera.pbj.runtime.ParseException;
 import java.util.List;
 
+/**
+ * Service that handles the verification of block items, it receives items from the handler.
+ */
 public interface BlockVerificationService {
+    /**
+     * Everytime the handler receives a block item, it will call this method to verify the block item.
+     *
+     * @param blockItems the block items to add to the verification service
+     * @throws ParseException if the block items are invalid
+     */
     void onBlockItemsReceived(List<BlockItemUnparsed> blockItems) throws ParseException;
 }
