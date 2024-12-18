576af16889a5f884d4ac78e4f63a8d5ff39dcf70
Alfredo Gutierrez <alfredo@swirldslabs.com>
2024-12-18 16:11:01 -0600

    Added env mappings for new verification config properties
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- server/src/main/java/com/hedera/block/server/verification/VerificationResult.java
+++ server/src/main/java/com/hedera/block/server/verification/VerificationResult.java
@@ -19,5 +19,12 @@ package com.hedera.block.server.verification;
 import com.hedera.pbj.runtime.io.buffer.Bytes;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
+/**
+ * A record representing the result of a block verification.
+ *
+ * @param blockNumber the block number
+ * @param blockHash the block hash
+ * @param status the verification status
+ */
 public record VerificationResult(
         @NonNull Long blockNumber, @NonNull Bytes blockHash, @NonNull BlockVerificationStatus status) {}
