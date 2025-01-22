8258aa56b93d009a9b3fb82ff37b2d84bda81791
Alfredo Gutierrez <alfredo@swirldslabs.com>
2025-01-21 20:36:44 -0600

    BlockManagerInjectionModule
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

---
+++ server/src/main/java/com/hedera/block/server/manager/BlockManagerInjectionModule.java
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+package com.hedera.block.server.manager;
+
+import com.hedera.block.server.notifier.Notifier;
+import com.hedera.block.server.persistence.storage.PersistenceStorageConfig;
+import com.hedera.block.server.verification.VerificationConfig;
+import dagger.Module;
+import dagger.Provides;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import javax.inject.Singleton;
+
+@Module
+public interface BlockManagerInjectionModule {
+
+    @Provides
+    @Singleton
+    static BlockManager provideBlockManager(
+            @NonNull final Notifier notifier,
+            @NonNull final PersistenceStorageConfig persistenceStorageConfig,
+            @NonNull final VerificationConfig verificationConfig) {
+
+        boolean skipPersistence = persistenceStorageConfig.type().equals(PersistenceStorageConfig.StorageType.NO_OP);
+        boolean skipVerification = verificationConfig.type().equals(VerificationConfig.VerificationServiceType.NO_OP);
+
+        return new BlockManagerImpl(notifier, skipPersistence, skipVerification);
+    }
+}
