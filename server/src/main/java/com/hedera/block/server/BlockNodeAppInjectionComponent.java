05843a8b2781ae27209d260d104d8e07e2b9224d
Alfredo Gutierrez <alfredo@swirldslabs.com>
2025-01-28 14:04:19 -0600

    Refactor BlockManager to AckHandler 2
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- server/src/main/java/com/hedera/block/server/BlockNodeAppInjectionComponent.java
+++ server/src/main/java/com/hedera/block/server/BlockNodeAppInjectionComponent.java
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.block.server;
 
-import com.hedera.block.server.ack.BlockManagerInjectionModule;
+import com.hedera.block.server.ack.AckHandlerInjectionModule;
 import com.hedera.block.server.config.ConfigInjectionModule;
 import com.hedera.block.server.health.HealthInjectionModule;
 import com.hedera.block.server.mediator.MediatorInjectionModule;
@@ -30,7 +30,7 @@ import javax.inject.Singleton;
             MetricsInjectionModule.class,
             PbjInjectionModule.class,
             VerificationInjectionModule.class,
-            BlockManagerInjectionModule.class
+            AckHandlerInjectionModule.class
         })
 public interface BlockNodeAppInjectionComponent {
     /**
