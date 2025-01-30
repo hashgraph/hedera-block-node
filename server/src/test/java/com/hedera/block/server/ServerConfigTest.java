afe7963d10ebcad244d45b823a34f4af064ede01
Matt Peterson <matt.peterson@swirldslabs.com>
2025-01-30 09:33:34 -0700

    fix: converted tests to parameterized tests
    Signed-off-by: Matt Peterson <matt.peterson@swirldslabs.com>

--- server/src/test/java/com/hedera/block/server/ServerConfigTest.java
+++ server/src/test/java/com/hedera/block/server/ServerConfigTest.java
@@ -4,9 +4,13 @@ package com.hedera.block.server;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ServerConfigTest {
 
@@ -25,67 +29,119 @@ class ServerConfigTest {
         assertEquals(8080, serverConfig.port());
     }
 
-    @Test
-    void testMessageSizeTooBig() {
+    @ParameterizedTest
+    @MethodSource("outOfRangeMaxMessageSizes")
+    void testMessageSizesOutOfBounds(final int messageSize, final String message) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new ServerConfig(16_777_216, 32_768, 32_768, 8080))
-                .withMessage(String.format(
-                        RANGE_ERROR_TEMPLATE,
-                        "server.maxMessageSizeBytes",
-                        16_777_216,
-                        ServerConfig.minMaxMessageSizeBytes,
-                        ServerConfig.maxMaxMessageSizeBytes));
+                .isThrownBy(() -> new ServerConfig(messageSize, 32_768, 32_768, 8080))
+                .withMessage(message);
     }
 
-    @Test
-    void testMessageSizeTooSmall() {
+    @ParameterizedTest
+    @MethodSource("outOfRangeSendBufferSizes")
+    void testSocketSendBufferSize(int sendBufferSize, String message) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new ServerConfig(10_239, 32_768, 32_768, 8080))
-                .withMessage(String.format(
-                        RANGE_ERROR_TEMPLATE,
-                        "server.maxMessageSizeBytes",
+                .isThrownBy(() -> new ServerConfig(4_194_304, sendBufferSize, 32_768, 8080))
+                .withMessage(message);
+    }
+
+    @ParameterizedTest
+    @MethodSource("outOfRangeReceiveBufferSizes")
+    void testSocketReceiveBufferSize(int receiveBufferSize, String message) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new ServerConfig(4_194_304, 32_768, receiveBufferSize, 8080))
+                .withMessage(message);
+    }
+
+    @ParameterizedTest
+    @MethodSource("outOfRangePorts")
+    void testPortValues(final int port, final String message) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new ServerConfig(4_194_304, 32_768, 32_768, port))
+                .withMessage(message);
+    }
+
+    private static Stream<Arguments> outOfRangePorts() {
+        return Stream.of(
+                Arguments.of(
+                        1023,
+                        String.format(
+                                RANGE_ERROR_TEMPLATE, "server.port", 1023, ServerConfig.minPort, ServerConfig.maxPort)),
+                Arguments.of(
+                        65_536,
+                        String.format(
+                                RANGE_ERROR_TEMPLATE,
+                                "server.port",
+                                65_536,
+                                ServerConfig.minPort,
+                                ServerConfig.maxPort)));
+    }
+
+    private static Stream<Arguments> outOfRangeReceiveBufferSizes() {
+        return Stream.of(
+                Arguments.of(
+                        32_767,
+                        String.format(
+                                RANGE_ERROR_TEMPLATE,
+                                "server.socketReceiveBufferSizeBytes",
+                                32_767,
+                                ServerConfig.minSocketReceiveBufferSizeBytes,
+                                Integer.MAX_VALUE)),
+                Arguments.of(
+                        1,
+                        String.format(
+                                RANGE_ERROR_TEMPLATE,
+                                "server.socketReceiveBufferSizeBytes",
+                                1,
+                                ServerConfig.minSocketReceiveBufferSizeBytes,
+                                Integer.MAX_VALUE)));
+    }
+
+    private static Stream<Arguments> outOfRangeSendBufferSizes() {
+        return Stream.of(
+                Arguments.of(
+                        32_767,
+                        String.format(
+                                RANGE_ERROR_TEMPLATE,
+                                "server.socketSendBufferSizeBytes",
+                                32_767,
+                                ServerConfig.minSocketSendBufferSizeBytes,
+                                Integer.MAX_VALUE)),
+                Arguments.of(
+                        1,
+                        String.format(
+                                RANGE_ERROR_TEMPLATE,
+                                "server.socketSendBufferSizeBytes",
+                                1,
+                                ServerConfig.minSocketSendBufferSizeBytes,
+                                Integer.MAX_VALUE)));
+    }
+
+    private static Stream<Arguments> outOfRangeMaxMessageSizes() {
+        return Stream.of(
+                Arguments.of(
+                        10_238,
+                        String.format(
+                                RANGE_ERROR_TEMPLATE,
+                                "server.maxMessageSizeBytes",
+                                10_238,
+                                ServerConfig.minMaxMessageSizeBytes,
+                                ServerConfig.maxMaxMessageSizeBytes)),
+                Arguments.of(
                         10_239,
-                        ServerConfig.minMaxMessageSizeBytes,
-                        ServerConfig.maxMaxMessageSizeBytes));
-    }
-
-    @Test
-    void testSocketSendBufferSizeTooSmall() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new ServerConfig(4_194_304, 32_767, 32_768, 8080))
-                .withMessage(String.format(
-                        RANGE_ERROR_TEMPLATE,
-                        "server.socketSendBufferSizeBytes",
-                        32_767,
-                        ServerConfig.minSocketSendBufferSizeBytes,
-                        Integer.MAX_VALUE));
-    }
-
-    @Test
-    void testSocketReceiveBufferSizeTooSmall() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new ServerConfig(4_194_304, 32_768, 32_767, 8080))
-                .withMessage(String.format(
-                        RANGE_ERROR_TEMPLATE,
-                        "server.socketReceiveBufferSizeBytes",
-                        32_767,
-                        ServerConfig.minSocketReceiveBufferSizeBytes,
-                        Integer.MAX_VALUE));
-    }
-
-    @Test
-    void testPortValueTooBig() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new ServerConfig(4_194_304, 32_768, 32_768, 65_536))
-                .withMessage(String.format(
-                        RANGE_ERROR_TEMPLATE, "server.port", 65_536, ServerConfig.minPort, ServerConfig.maxPort));
-    }
-
-    @Test
-    void testPortValueTooSmall() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new ServerConfig(4_194_304, 32_768, 32_768, 1023))
-                .withMessage(String.format(
-                        RANGE_ERROR_TEMPLATE, "server.port", 1023, ServerConfig.minPort, ServerConfig.maxPort));
+                        String.format(
+                                RANGE_ERROR_TEMPLATE,
+                                "server.maxMessageSizeBytes",
+                                10_239,
+                                ServerConfig.minMaxMessageSizeBytes,
+                                ServerConfig.maxMaxMessageSizeBytes)),
+                Arguments.of(
+                        16_777_216,
+                        String.format(
+                                RANGE_ERROR_TEMPLATE,
+                                "server.maxMessageSizeBytes",
+                                16_777_216,
+                                ServerConfig.minMaxMessageSizeBytes,
+                                ServerConfig.maxMaxMessageSizeBytes)));
     }
 }
