a88b1c2aacdf76c39d1db20e9d1c2041bac9e78c
Matt Peterson <matt.peterson@swirldslabs.com>
2025-01-31 10:07:42 -0700

    fix: added debug logging to NoOpBlockVerificationService
    Signed-off-by: Matt Peterson <matt.peterson@swirldslabs.com>

--- server/src/main/java/com/hedera/block/server/verification/service/NoOpBlockVerificationService.java
+++ server/src/main/java/com/hedera/block/server/verification/service/NoOpBlockVerificationService.java
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.block.server.verification.service;
 
+import static java.lang.System.Logger.Level.DEBUG;
 import static java.lang.System.Logger.Level.INFO;
 
 import com.hedera.hapi.block.BlockItemUnparsed;
@@ -9,12 +10,12 @@ import java.util.List;
 
 /** No-op implementation of the {@link BlockVerificationService}. */
 public class NoOpBlockVerificationService implements BlockVerificationService {
+    final System.Logger LOGGER = System.getLogger(getClass().getName());
 
     /**
      * Constructs a no-op block verification service.
      */
     public NoOpBlockVerificationService() {
-        System.Logger LOGGER = System.getLogger(getClass().getName());
         LOGGER.log(INFO, "Using " + getClass().getName());
     }
 
@@ -23,6 +24,6 @@ public class NoOpBlockVerificationService implements BlockVerificationService {
      */
     @Override
     public void onBlockItemsReceived(@NonNull List<BlockItemUnparsed> blockItems) {
-        // no-op
+        LOGGER.log(DEBUG, "{0} is ignoring {1} block items.", getClass().getName(), blockItems.size());
     }
 }
