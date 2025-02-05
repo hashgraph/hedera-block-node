f0f2aefdf03e67ff2d1c4b066795d3d3d0d3c6a3
Matt Peterson <matt.peterson@swirldslabs.com>
2025-02-05 11:01:07 -0700

    fix: removed UTC timezone flags. we can rely on the linux default
    Signed-off-by: Matt Peterson <matt.peterson@swirldslabs.com>

--- server/docker/update-env.sh
+++ server/docker/update-env.sh
@@ -38,9 +38,8 @@ else
   echo "JAVA_OPTS='-Xms16G -Xmx16G'" >> .env
 fi
 
-# Set the timezone to UTC and the logging properties file
 # file is mounted in the docker-compose.yml, changes to the file will be reflected in the container by simply restarting it
-echo "JAVA_TOOL_OPTIONS='-Djava.util.logging.config.file=/app/logs/config/logging.properties -Duser.timezone=UTC'" >> .env
+echo "JAVA_TOOL_OPTIONS='-Djava.util.logging.config.file=/app/logs/config/logging.properties'" >> .env
 
 # Output the values
 echo ".env properties:"
