ef164411a880908c5755f84e9ea3ba7591058f51
georgi-l95 <glazarov95@gmail.com>
2025-02-17 16:15:00 +0200

    craft blocks with "valid" signature
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- simulator/src/main/java/com/hedera/block/simulator/generator/CraftBlockStreamManager.java
+++ simulator/src/main/java/com/hedera/block/simulator/generator/CraftBlockStreamManager.java
@@ -1,14 +1,19 @@
 package com.hedera.block.simulator.generator;
 
 import com.google.protobuf.ByteString;
+import com.hedera.block.common.hasher.Hashes;
+import com.hedera.block.common.hasher.HashingUtilities;
+import com.hedera.block.common.hasher.NaiveStreamingTreeHasher;
 import com.hedera.block.common.hasher.StreamingTreeHasher;
 import com.hedera.block.simulator.config.data.BlockGeneratorConfig;
 import com.hedera.block.simulator.config.types.GenerationMode;
 import com.hedera.block.simulator.exception.BlockSimulatorParsingException;
+import com.hedera.hapi.block.BlockItemUnparsed;
 import com.hedera.hapi.block.stream.output.protoc.BlockHeader;
 import com.hedera.hapi.block.stream.protoc.Block;
 import com.hedera.hapi.block.stream.protoc.BlockItem;
 import com.hedera.hapi.block.stream.protoc.BlockProof;
+import com.hedera.pbj.runtime.ParseException;
 import com.hedera.pbj.runtime.io.buffer.Bytes;
 import com.hederahashgraph.api.proto.java.BlockHashAlgorithm;
 import com.hederahashgraph.api.proto.java.SemanticVersion;
@@ -32,18 +37,24 @@ public class CraftBlockStreamManager implements BlockStreamManager{
 
     // State
     private final GenerationMode generationMode;
-    private final ByteString blockSignature;
-    private final ByteString previousStateRootHash;
-    private ByteString previousBlockHash;
+    private final byte[] previousStateRootHash;
+    private byte[] previousBlockHash;
+    private byte[] currentBlockHash;
     private long currentBlockNumber;
+    private StreamingTreeHasher inputTreeHasher;
+    private StreamingTreeHasher outputTreeHasher;
 
     public CraftBlockStreamManager(@NonNull BlockGeneratorConfig blockGeneratorConfig) {
         this.blockGeneratorConfig = requireNonNull(blockGeneratorConfig);
         this.generationMode = this.blockGeneratorConfig.generationMode();
+        this.previousStateRootHash = new byte[StreamingTreeHasher.HASH_LENGTH];
+
+        this.previousBlockHash = new byte[StreamingTreeHasher.HASH_LENGTH];
+        this.currentBlockHash = new byte[StreamingTreeHasher.HASH_LENGTH];
+
         this.currentBlockNumber = 0;
-        this.blockSignature = ByteString.copyFrom(new byte[StreamingTreeHasher.HASH_LENGTH]);
-        this.previousBlockHash = ByteString.copyFrom(new byte[StreamingTreeHasher.HASH_LENGTH]);
-        this.previousStateRootHash = ByteString.copyFrom(new byte[StreamingTreeHasher.HASH_LENGTH]);
+        this.inputTreeHasher = new NaiveStreamingTreeHasher();
+        this.outputTreeHasher = new NaiveStreamingTreeHasher();
 
         LOGGER.log(INFO, "Block Stream Simulator will use Craft mode for block management.");
     }
@@ -60,28 +71,65 @@ public class CraftBlockStreamManager implements BlockStreamManager{
 
     @Override
     public Block getNextBlock() throws IOException, BlockSimulatorParsingException {
-        LOGGER.log(DEBUG, "Started creation of block number %s.".formatted(this.currentBlockNumber));
+        LOGGER.log(DEBUG, "Started creation of block number %s.".formatted(currentBlockNumber));
         List<BlockItem> blockItems = new ArrayList<>();
+        List<BlockItemUnparsed> blockItemsUnparsed = new ArrayList<>();
+
         BlockItem headerItem = BlockItem.newBuilder()
                 .setBlockHeader(createBlockHeader())
                 .build();
         blockItems.add(headerItem);
 
+        // create all other block items with transactions and add them
+        LOGGER.log(DEBUG, "Appending %s number of block items in this block.".formatted(currentBlockNumber));
+        for (BlockItem blockItem : blockItems) {
+            BlockItemUnparsed blockItemUnparsed = unparseBlockItem(blockItem);
+            blockItemsUnparsed.add(blockItemUnparsed);
+        }
+        processBlockItems(blockItemsUnparsed);
+        updateCurrentBlockHash();
+
         // create random number of block items with random type of transactions depending on type from configuration
-        LOGGER.log(DEBUG, "Appending %s number of block items in this block.".formatted(this.currentBlockNumber));
         BlockItem proofItem = BlockItem.newBuilder()
                 .setBlockProof(createBlockProof())
                 .build();
         blockItems.add(proofItem);
-
-        // depending on the block items calculate new current block hash
-        // depending on the block items those calculate new state root hash
-        incrementBlock();
+        resetState();
         return Block.newBuilder().addAllItems(blockItems).build();
     }
 
-    private void incrementBlock() {
-        this.currentBlockNumber++;
+    private void updateCurrentBlockHash() {
+        com.hedera.hapi.block.stream.BlockProof unfinishedBlockProof = com.hedera.hapi.block.stream.BlockProof.newBuilder()
+                .previousBlockRootHash(Bytes.wrap(previousBlockHash))
+                .startOfBlockStateRootHash(Bytes.wrap(previousStateRootHash))
+                .build();
+
+        currentBlockHash = HashingUtilities.computeFinalBlockHash(unfinishedBlockProof, inputTreeHasher, outputTreeHasher).toByteArray();
+    }
+
+    private BlockItemUnparsed unparseBlockItem(BlockItem blockItem) throws BlockSimulatorParsingException {
+        try {
+            return BlockItemUnparsed.PROTOBUF.parse(Bytes.wrap(blockItem.toByteArray()));
+        } catch (ParseException e) {
+            throw new BlockSimulatorParsingException(e);
+        }
+    }
+
+    private void processBlockItems(List<BlockItemUnparsed> blockItems) {
+        Hashes hashes = HashingUtilities.getBlockHashes(blockItems);
+        while (hashes.inputHashes().hasRemaining()) {
+            inputTreeHasher.addLeaf(hashes.inputHashes());
+        }
+        while (hashes.outputHashes().hasRemaining()) {
+            outputTreeHasher.addLeaf(hashes.outputHashes());
+        }
+    }
+
+    private void resetState() {
+        inputTreeHasher = new NaiveStreamingTreeHasher();
+        outputTreeHasher = new NaiveStreamingTreeHasher();
+        currentBlockNumber++;
+        previousBlockHash = currentBlockHash;
     }
 
     private BlockHeader createBlockHeader() {
@@ -92,16 +140,21 @@ public class CraftBlockStreamManager implements BlockStreamManager{
                 .setSoftwareVersion(semanticVersion)
                 .setHashAlgorithm(BlockHashAlgorithm.SHA2_384)
                 .setFirstTransactionConsensusTime(firstTransactionConsensusTime)
-                .setPreviousBlockHash(this.previousBlockHash)
-                .setNumber(this.currentBlockNumber).build();
+                .setPreviousBlockHash(ByteString.copyFrom(previousBlockHash))
+                .setNumber(currentBlockNumber)
+                .build();
     }
 
     private BlockProof createBlockProof() {
         return BlockProof.newBuilder()
-                .setBlock(this.currentBlockNumber)
-                .setPreviousBlockRootHash(this.previousBlockHash)
-                .setStartOfBlockStateRootHash(this.previousStateRootHash)
-                .setBlockSignature(this.blockSignature)
+                .setBlock(currentBlockNumber)
+                .setPreviousBlockRootHash(ByteString.copyFrom(previousBlockHash))
+                .setStartOfBlockStateRootHash(ByteString.copyFrom(previousStateRootHash))
+                .setBlockSignature(produceSignature())
                 .build();
+    }
+
+    private ByteString produceSignature() {
+        return ByteString.copyFrom(HashingUtilities.noThrowSha384HashOf(currentBlockHash));
     }
 }
