ce726dee4a0beafc0d411faf54bb4bb5d7c5658d
Matt Peterson <matt.peterson@swirldslabs.com>
2025-02-18 15:12:28 -0700

    fix: removed Objects.requireNonNull() checks
    Signed-off-by: Matt Peterson <matt.peterson@swirldslabs.com>

--- server/src/main/java/com/hedera/block/server/consumer/ConsumerStreamResponseObserver.java
+++ server/src/main/java/com/hedera/block/server/consumer/ConsumerStreamResponseObserver.java
@@ -141,8 +141,7 @@ class ConsumerStreamResponseObserver implements BlockNodeEventHandler<ObjectEven
             throw new IllegalArgumentException(message);
         }
 
-        final List<BlockItemUnparsed> blockItems =
-                Objects.requireNonNull(subscribeStreamResponse.blockItems()).blockItems();
+        final List<BlockItemUnparsed> blockItems = subscribeStreamResponse.blockItems().blockItems();
 
         // Only start sending BlockItems after we've reached
         // the beginning of a block.
@@ -154,7 +153,7 @@ class ConsumerStreamResponseObserver implements BlockNodeEventHandler<ObjectEven
         if (streamStarted) {
             if (firstBlockItem.hasBlockHeader()) {
                 long blockNumber = BlockHeader.PROTOBUF
-                        .parse(Objects.requireNonNull(firstBlockItem.blockHeader()))
+                        .parse(firstBlockItem.blockHeader())
                         .number();
                 if (LOGGER.isLoggable(DEBUG)) {
                     LOGGER.log(DEBUG, "{0} sending block: {1}", Thread.currentThread(), blockNumber);
